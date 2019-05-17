467366ea3b34c93e4ec43b3027d245a578f13223
Benoit Knuchel <benoitknuchel@hotmail.com>
2019-05-17 13:19:07 +0200

    to PAM

--- BinaryToCodeword.py
+++ BinaryToCodeword.py
@@ -1,45 +1,15 @@
-##### Call 'f(String)' to translate a binary string to a 4-QAM code, returns a list of pairs '(+-1, +-1)'
+##### Call 'f(String)' to translate a binary string to a PAM code, returns a list of pairs '+-1'
 
-# 4-QAM : c0 = (1,1), c1 = (1, -1), c2 = (-1, -1), c3 = (-1, 1)
-# 00->c0, 01->c1, 10->c2, 11->c3
-
-c0 = (1,1)
-c1 = (1,-1)
-c2 = (-1,-1)
-c3 = (-1,1)
+# 1 -> -1, 0 -> 1
 
 def f(s):
-    lBinPair = makePair(s) #add bits by pair in lBinPair
-    
-    lres = changeToCodeword(lBinPair)
-    
-    return lres
-    
-def makePair(s):
-    lTemp = []
-    counter = 0
-    temp = -1
-    for bit in s:
-        counter += 1
-        if(counter%2 != 0):
-            temp = bit
+    string = ""
+    temp = "" 
+    for c in s:
+        if(c == '1'):
+            c = '-1'
         else:
-            lTemp.append((temp, bit))
-            temp = -1
-    return lTemp
-    
-def changeToCodeword(l):
-    lTemp = []
-    for x in l: #change pair to respective codewords
-        lTemp.append(pairToCodeword(x))
-    return lTemp
-    
-def pairToCodeword(x):
-    if(x == ('0','0')):
-        return c0
-    elif(x == ('0','1')):
-        return c1
-    elif(x == ('1','0')):
-        return c2
-    else:
-        return c3;
+            c = '1'    
+        temp = string + c
+        string = temp
+    return string
--- CodewordToBinary.py
+++ CodewordToBinary.py
@@ -1,22 +1,20 @@
-##### Call 'f(String)' to translate a 4-QAM code to a binary string, return a binary string
+##### Call 'f(String)' to translate a PAM code to a binary string, return a binary string
 
-# 4-QAM : c0 = (1,1), c1 = (1, -1), c2 = (-1, -1), c3 = (-1, 1)
-# 00->c0, 01->c1, 10->c2, 11->c3
+# -1 -> 1, 1 -> 0
 
-def f(codes):
-    lTemp = []
-    for pair in codes:
-        lTemp.append(codewordToBinary(pair))
-    
-    return "".join(lTemp)
-    
-    
-def codewordToBinary(x):
-    if(x == (1,1)):
-        return "00"
-    elif(x == (1,-1)):
-        return "01"
-    elif(x == (-1,-1)):
-        return "10"
-    else:
-        return "11";
+def f(s):
+    jump = False
+    string = ""
+    temp = ""
+    for c in s:
+        if(jump):
+            jump = False
+            continue
+        if(c == '1'):
+            c = '0'
+        elif(c == '-'):
+            c = '1'
+            jump = True
+        temp = string + c
+        string = temp
+    return string 
